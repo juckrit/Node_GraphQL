eb20428ae470461ddcd70fe6276f6f3324fb6b1f
Juckrit <juckrit072@gmail.com>
2020-03-06 09:31:28 +0700

    add lodash, can query by id

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,27 @@
 const graphql = require('graphql')
+// Load the full build.
+var _ = require('lodash');
+// Load the core build.
+var _ = require('lodash/core');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require('lodash/fp');
+ 
+// Load method categories.
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+ 
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN');
 
 
 //dummy data
- var userdata = [
-     {id:1,name:"user1",age:11},
-     {id:2,name:"user2",age:22},
-     {id:3,name:"user3",age:33},
-     {id:4,name:"user4",age:44},
-     {id:5,name:"user5",age:55},
+ var usersData = [
+     {id:"1",name:"user1",age:11},
+     {id:"2",name:"user2",age:22},
+     {id:"3",name:"user3",age:33},
+     {id:"4",name:"user4",age:44},
+     {id:"5",name:"user5",age:55},
  ]
 
 const{
@@ -42,12 +56,13 @@ const RootQuery = new GraphQLObjectType({
             args:{id:    {type : GraphQLString}},
 
             resolve(parent,args){
-                let user = {
-                    id: "123",
-                    name: "john",
-                    age: 13
-                }
-                return user
+                // let user = {
+                //     id: "123",
+                //     name: "john",
+                //     age: 13
+                // }
+                // return user
+                return _.find(usersData,{id: args.id})
 
                 //We resolve with data
                 //get and retrun data from datasource
